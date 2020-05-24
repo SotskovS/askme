a7df11487a83ae58e70625957977a3d4407a9a77
SotskovS <svalgele@gmail.com>
2020-05-24 22:39:00 +0300

    Refactoring user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   ITERATIONS = 2000
   DIGEST = OpenSSL::Digest::SHA256.new
   REGEXP_EMAIL = /[\w\.]+@[\w\.]+/
-  REGEXP_USERNAME = /[a-zA-Z0-9_]+/
+  REGEXP_USERNAME = /\w/
 
   attr_accessor :password
 
@@ -16,16 +16,6 @@ class User < ApplicationRecord
   validates :password, presence: true, on: :create
   validates :password, confirmation: true
 
-  before_save :encrypt_password
-
-  def encrypt_password
-    if self.password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-      self.password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
-          self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
-        )
-    end
-  end
 
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
@@ -38,4 +28,17 @@ class User < ApplicationRecord
     return user if user.password_hash == hashed_password
     nil
   end
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
+          self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+        )
+    end
+  end
+
+  private
+
+  before_save :encrypt_password
 end
