0325ad7f57581f32dc3e1d69f40706df521398d7
SotskovS <svalgele@gmail.com>
2020-05-24 21:45:09 +0300

    Initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
 
   def show
-    @hello = "Привет, Rails"
+  	@user = User.new(
+  		name: 'Vasya',
+  	  username: 'inst',
+      avatar_url: nil
+  	)
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('24.05.2020')),
+      Question.new(text: 'В чем смысл?', created_at: Date.parse('24.05.2020'))
+    ]
   end
 end
