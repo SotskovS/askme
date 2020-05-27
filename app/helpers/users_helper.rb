51ec269f848714307083e18a1df7fe02d1d7faa2
SotskovS <svalgele@gmail.com>
2020-05-27 23:35:06 +0300

    Add helper (declension)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+	def declension(num)
+    num_declesion = num % 10
+  	if num_declesion == 0 || num_declesion.between?(5,9) || num % 100 >= 11 && num % 100 < 20
+      'вопросов'
+    elsif num_declesion == 1
+      'вопрос'
+    else
+      'вопроса'
+    end
+  end
 end
