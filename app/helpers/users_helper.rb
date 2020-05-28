ba2d5063d3ebfbbf88fc9f0a6589774e3f783a03
SotskovS <svalgele@gmail.com>
2020-05-28 23:39:42 +0300

    spaces

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def declension(num, form_1, form_2, form_3)
     num_declesion = num % 10
-  	if num_declesion == 0 || num_declesion.between?(5,9) || num % 100 >= 11 && num % 100 < 20
+    if num_declesion == 0 || num_declesion.between?(5,9) || num % 100 >= 11 && num % 100 < 20
       form_3
     elsif num_declesion == 1
       form_1
