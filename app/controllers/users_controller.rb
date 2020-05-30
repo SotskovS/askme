d03f3d288cc539535fe0aaa1bc68c3d31dcf6612
SotskovS <svalgele@gmail.com>
2020-05-30 22:53:36 +0300

    Revert "User new (login)"
    This reverts commit ca412a23c74938b87d6fa131d2d78a7945473e7a.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,35 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/' \
+          '71269686e0f757ddb4f73614f43ae445?s=100'
+      ),
+      User.new(id: 2, name: 'Misha', username: 'aristofun')
+    ]
   end
 
   def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    @user.save
   end
 
   def edit
   end
 
   def show
-  end
+  	@user = User.new(
+  		name: 'Vasya',
+  	  username: 'insd',
+      avatar_url: nil
+  	)
 
-private
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation,
-                                 :name, :username, :avatar_url)
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('24.05.2020')),
+      Question.new(text: 'Где мой броневичок?', created_at: Date.parse('27.05.2020'))
+    ]
+
+    @new_question = Question.new
   end
 end
